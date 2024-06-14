874a6755b999e3b57eba825e92d38c7ab629ee3f
finanalyst <rnhainsworth@gmail.com>
2024-06-14 16:26:21 +0100

    pull in raw file, not HTMLized

--- Build.pm
+++ Build.pm
@@ -1,5 +1,5 @@
 use v6.d;
 'rakudociem-ipsum.rakudoc'.IO.unlink if 'rakudociem-ipsum.rakudoc'.IO ~~ :e & :f;
 'wget-log'.IO.unlink if 'wget-log'.IO ~~ :e & :f;
-my $proc = run <wget https://github.com/Raku/RakuDoc-GAMMA/blob/main/rakudociem-ipsum.rakudoc>,:err,:out;
+my $proc = run <wget https://github.com/Raku/RakuDoc-GAMMA/raw/main/rakudociem-ipsum.rakudoc>,:err,:out;
 say 'check wget-log if rakudociem-ipsum.rakudoc is not present'
