b664658549727332083a7d617afb885ed1712d11
Coleman McFarland <coleman@de.limited>
2024-07-20 11:20:25 -0400

    Add bin/test-script.sh and remove stray .gitmodules

---
+++ bin/test-script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Test script.
+#
+# Meant to be invoked from within a clean container, so we
+# can make sure we don't depend on any local state in CI.
+#
+# Example:
+#
+#     podman run -v $PWD:/src -w /src docker.io/rakudo-star:latest sh ./bin/test-script.sh
+#
+set -e
+apt-get update -y
+apt-get install -y build-essential
+
+zef install --deps-only .
+
