d47f67f9933bcb49488971cc9acd8e069fe0b616
Coleman McFarland <coleman@de.limited>
2024-12-31 19:58:13 -0500

    Add prove6 invocation to bin/test-script.sh
    This is an unoptimized "do everything" script, but it is enough to
    get CI started.

--- bin/test-script.sh
+++ bin/test-script.sh
@@ -10,11 +10,17 @@
 #     podman run -v $PWD:/src -w /src docker.io/rakudo-star:latest sh ./bin/test-script.sh
 #
 set -e
+echo "Installing system dependencies."
 apt-get update -y
 apt-get install -y build-essential
 
+echo "Installing Raku library dependencies."
 zef install --deps-only .
-
 zef install -/precompile-install .
 
+# prove6 is pre-installed in the open source rakudo-star image
+echo "Running tests with prove6."
+prove6 xt/*
+
+echo "Done."
 
